9e0c9895ad96dfd1119442d2e4a179e7de6ac93f
Ayomide <ayomidebankole1809@gmail.com>
2025-10-11 22:13:53 +0100

    aadd

--- script.js
+++ script.js
@@ -1,146 +1,186 @@
-// const post = {
+// // const post = {
+// //   id: 1,
+// //   title: "Post One",
+// //   body: "This is the body",
+// // };
+
+// // // convert to json string
+// // const str = JSON.stringify(post);
+
+// // // Parse JSON
+// // const obj = JSON.parse(str);
+
+// // // const posts = [
+// // //   {
+// // //     id: "1",
+// // //     title: "Post One",
+// // //     body: "This is the body",
+// // //   },
+// // //   {
+// // //     id: "2 ",
+// // //     title: "Take out Trash",
+// // //   },
+// // // ];
+
+// // // const str2 = JSON.stringify(posts);
+
+// // // // console.log(str2);
+
+// // // // Object Challenge
+
+// // // // step 1
+// // // const library = [
+// // //   {
+// // //     title: "THe Road Ahead",
+// // //     author: "Bill gates",
+// // //     status: {
+// // //       own: true,
+// // //       reading: false,
+// // //       read: false,
+// // //     },
+// // //   },
+// // //   {
+// // //     title: "Steve jobs",
+// // //     author: "walter Isaacson",
+// // //     status: {
+// // //       own: true,
+// // //       reading: false,
+// // //       read: false,
+// // //     },
+// // //   },
+// // //   {
+// // //     title: "Mockingjay",
+// // //     author: "Suzane collins",
+// // //     status: {
+// // //       own: true,
+// // //       reading: false,
+// // //       read: false,
+// // //     },
+// // //   },
+// // // ];
+
+// // // // step 2
+// // // library[0].status.read = true;
+// // // library[1].status.read = true;
+// // // library[2].status.read = true;
+
+// // // console.log(library);
+
+// // // // // step 3
+// // // const { title: firstBook } = library[0];
+
+// // // console.log(firstBook);
+
+// // // // // step 4
+// // // const libraryJSON = JSON.stringify(library);
+
+// // // console.log(libraryJSON);
+
+// // function sayHello() {
+// //   console.log("hello world");
+// // }
+
+// // sayHello();
+
+// // function add(num1, num2) {
+// //   console.log(num1 + num2);
+// // }
+
+// // add(5, 10);
+
+// // function subtract(num1, num2) {
+// //   return num1 - num2;
+
+// //   console.log("After the return");
+// // }
+
+// // const result = subtract(10, 2);
+
+// // console.log(result, subtract(20, 5));
+
+// function registerUser(user = "tolu") {
+//   return user + " is registered";
+// }
+// console.log(registerUser());
+
+// // Rest Params
+// function sum(...numbers) {
+//   let total = 0;
+
+//   for (const num of numbers) {
+//     total += num;
+//   }
+
+//   return total;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 6, 100, 10));
+
+// // objects as params
+// function loginUser(user) {
+//   return `The user ${user.name} with the id of ${user.id} is logged in`;
+// }
+
+// const user = {
 //   id: 1,
-//   title: "Post One",
-//   body: "This is the body",
+//   name: "steve",
 // };
 
-// // convert to json string
-// const str = JSON.stringify(post);
+// console.log(loginUser(user));
+// console.log(
+//   loginUser({
+//     id: 2,
+//     name: "saraki",
+//   })
+// );
 
-// // Parse JSON
-// const obj = JSON.parse(str);
+// // Arrays as params
+// function getRandom(...arr) {
+//   const randomIndex = Math.floor(Math.random() * arr.length);
 
-// // const posts = [
-// //   {
-// //     id: "1",
-// //     title: "Post One",
-// //     body: "This is the body",
-// //   },
-// //   {
-// //     id: "2 ",
-// //     title: "Take out Trash",
-// //   },
-// // ];
+//   const item = arr[randomIndex];
 
-// // const str2 = JSON.stringify(posts);
-
-// // // console.log(str2);
-
-// // // Object Challenge
-
-// // // step 1
-// // const library = [
-// //   {
-// //     title: "THe Road Ahead",
-// //     author: "Bill gates",
-// //     status: {
-// //       own: true,
-// //       reading: false,
-// //       read: false,
-// //     },
-// //   },
-// //   {
-// //     title: "Steve jobs",
-// //     author: "walter Isaacson",
-// //     status: {
-// //       own: true,
-// //       reading: false,
-// //       read: false,
-// //     },
-// //   },
-// //   {
-// //     title: "Mockingjay",
-// //     author: "Suzane collins",
-// //     status: {
-// //       own: true,
-// //       reading: false,
-// //       read: false,
-// //     },
-// //   },
-// // ];
-
-// // // step 2
-// // library[0].status.read = true;
-// // library[1].status.read = true;
-// // library[2].status.read = true;
-
-// // console.log(library);
-
-// // // // step 3
-// // const { title: firstBook } = library[0];
-
-// // console.log(firstBook);
-
-// // // // step 4
-// // const libraryJSON = JSON.stringify(library);
-
-// // console.log(libraryJSON);
-
-// function sayHello() {
-//   console.log("hello world");
+//   console.log(item);
 // }
 
-// sayHello();
+// getRandom(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-// function add(num1, num2) {
-//   console.log(num1 + num2);
+// global and function scope
+// alert("are you sure you want to open this page");
+// console.log(window.innerWidth);
+
+// const x = 100;
+
+// console.log(x, "in global");
+
+// function run() {
+//   console.log(window.innerHeight);
+//   console.log(x, "in function");
 // }
 
-// add(5, 10);
+// run();
 
-// function subtract(num1, num2) {
-//   return num1 - num2;
-
-//   console.log("After the return");
+// if (true) {
+//   console.log(x, "in block");
 // }
 
-// const result = subtract(10, 2);
+// function add() {
+//   const x = 1;
+//   const y = 50;
+//   console.log(x + y);
+// }
 
-// console.log(result, subtract(20, 5));
+// add();
 
-function registerUser(user = "tolu") {
-  return user + " is registered";
-}
-console.log(registerUser());
+const x = 100;
 
-// Rest Params
-function sum(...numbers) {
-  let total = 0;
-
-  for (const num of numbers) {
-    total += num;
-  }
-
-  return total;
+if (true) {
+  const y = 200;
+  console.log(x + y);
 }
 
-console.log(sum(1, 2, 3, 4, 5, 6, 100, 10));
+// console.log(x + y);
 
-// objects as params
-function loginUser(user) {
-  return `The user ${user.name} with the id of ${user.id} is logged in`;
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
 }
-
-const user = {
-  id: 1,
-  name: "steve",
-};
-
-console.log(loginUser(user));
-console.log(
-  loginUser({
-    id: 2,
-    name: "saraki",
-  })
-);
-
-// Arrays as params
-function getRandom(...arr) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-
-  const item = arr[randomIndex];
-
-  console.log(item);
-}
-
-getRandom(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
